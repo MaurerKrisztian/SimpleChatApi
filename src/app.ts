53cb5ee772f4958fcafa054b879e0fe9092dfebc
Krisztián Maurer <maurerkrisztian@gmail.com>
2021-09-12 15:04:18 +0200

    port from env

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ MyLogger.logger.level = "trace"
 
 const server = Server.createApp();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     MyLogger.debug("App listening on port " + PORT);
 });
