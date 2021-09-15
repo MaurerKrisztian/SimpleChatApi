94a5ba7f760b4f910f9a36df91446a4e3690801e
Maurer Krisztian <maurerkrisztian>
2021-09-15 19:11:51 +0200

    fix text color, date format

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -4,87 +4,106 @@ const roomName = document.getElementById('room-name');
 const userList = document.getElementById('users');
 
 // Get username and room from URL
-const { username, room } = Qs.parse(location.search, {
-  ignoreQueryPrefix: true,
+const {username, room} = Qs.parse(location.search, {
+    ignoreQueryPrefix: true,
 });
 
 const socket = io();
 
 // Join chatroom
-socket.emit('joinRoom', { username, room });
+socket.emit('joinRoom', {username, room});
 
 // Get room and users
-socket.on('roomUsers', ({ room, users }) => {
-  outputRoomName(room);
-  outputUsers(users);
+socket.on('roomUsers', ({room, users}) => {
+    outputRoomName(room);
+    outputUsers(users);
 });
 
 // Message from server
 socket.on('message', (message) => {
-  console.log(message);
-  outputMessage(message);
+    console.log(message);
+    outputMessage(message);
 
-  // Scroll down
-  chatMessages.scrollTop = chatMessages.scrollHeight;
+    // Scroll down
+    chatMessages.scrollTop = chatMessages.scrollHeight;
 });
 
 // Message submit
 chatForm.addEventListener('submit', (e) => {
-  e.preventDefault();
+    e.preventDefault();
 
-  // Get message text
-  let msg = e.target.elements.msg.value;
+    // Get message text
+    let msg = e.target.elements.msg.value;
 
-  msg = msg.trim();
+    msg = msg.trim();
 
-  if (!msg) {
-    return false;
-  }
+    if (!msg) {
+        return false;
+    }
 
-  // Emit message to server
-  socket.emit('chatMessage', msg);
+    // Emit message to server
+    socket.emit('chatMessage', msg);
 
-  // Clear input
-  e.target.elements.msg.value = '';
-  e.target.elements.msg.focus();
+    // Clear input
+    e.target.elements.msg.value = '';
+    e.target.elements.msg.focus();
 });
 
 // Output message to DOM
 function outputMessage(message) {
-  const div = document.createElement('div');
-  div.classList.add('message');
-  const p = document.createElement('p');
-  p.classList.add('meta');
-  p.innerText = message.username;
-  p.innerHTML += `<span>${message.time}</span>`;
-  div.appendChild(p);
-  const para = document.createElement('p');
-  para.classList.add('text');
-  para.innerText = message.text;
-  div.appendChild(para);
-  document.querySelector('.chat-messages').appendChild(div);
+    const div = document.createElement('div');
+    div.classList.add('message');
+    const p = document.createElement('p');
+    p.classList.add('meta');
+    p.innerText = message.username;
+    p.innerHTML += ` <span>${formatDate(message.time)}</span>`;
+    div.appendChild(p);
+    const para = document.createElement('p');
+    para.classList.add('text');
+    para.innerText = message.text;
+    div.appendChild(para);
+    document.querySelector('.chat-messages').appendChild(div);
 }
 
 // Add room name to DOM
 function outputRoomName(room) {
-  roomName.innerText = room;
+    roomName.innerText = room;
 }
 
 // Add users to DOM
 function outputUsers(users) {
-  userList.innerHTML = '';
-  users.forEach((user) => {
-    const li = document.createElement('li');
-    li.innerText = user.username;
-    userList.appendChild(li);
-  });
+    userList.innerHTML = '';
+    users.forEach((user) => {
+        const li = document.createElement('li');
+        li.innerText = user.username;
+        userList.appendChild(li);
+    });
 }
 
 //Prompt the user before leave chat room
 document.getElementById('leave-btn').addEventListener('click', () => {
-  const leaveRoom = confirm('Are you sure you want to leave the chatroom?');
-  if (leaveRoom) {
-    window.location = '../index.html';
-  } else {
-  }
+    const leaveRoom = confirm('Are you sure you want to leave the chatroom?');
+    if (leaveRoom) {
+        window.location = '../index.html';
+    } else {
+    }
 });
+
+function formatDate(timestamp) {
+
+    // Create a date object from the timestamp
+    const date = new Date(timestamp);
+
+    // Create a list of names for the months
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+    const addZero = (i) => {
+        if (i < 10) {
+            i = "0" + i;
+        }
+        return i;
+    }
+
+    // return a formatted date
+    return months[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear() + " " + addZero(date.getHours()) + ":" + addZero(date.getMinutes());
+};
